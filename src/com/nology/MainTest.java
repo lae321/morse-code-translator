45cc0335256e9cf58dd792a1bfbf88082519a93f
lae321 <lae321@hotmail.co.uk>
2022-05-16 11:50:56 +0100

    Removed test case

--- src/com/nology/MainTest.java
+++ src/com/nology/MainTest.java
@@ -6,13 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
-  @Test
-  @DisplayName("Stuff")
-  void translate() {
-    assertEquals("sos", MorseCodeTranslator.translate("... --- ..."));
-  }
-
   @Test
   @DisplayName("Translate SOS")
   void translate() {
